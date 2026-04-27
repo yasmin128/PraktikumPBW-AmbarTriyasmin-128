d069edcae303ad717a47698e784b910ccb64ff35
yasmin128 <2410631170128@student.unsika.ac.id>
2026-04-27 14:23:16 +0700

    Pertemuan 10 & Update Fitur

--- Pertemuan10/index.php
+++ Pertemuan10/index.php
@@ -1,5 +1,13 @@
 <?php include 'proses_index.php'; ?>
 
+<?php
+session_start();
+if (!isset($_SESSION['login_Un51k4'])) {
+  header("Location: login.php?message=" . urlencode("Mengakses fitur harus login dulu bro."));
+  exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
--- Pertemuan10/nav.php
+++ Pertemuan10/nav.php
@@ -24,6 +24,9 @@
         <li class="nav-item">
           <a class="nav-link" href="hapus.php">Hapus Buku</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">Logout</a>
+        </li>
       </ul>
     </div>
   </div>
